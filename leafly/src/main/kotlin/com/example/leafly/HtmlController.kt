2d1921bb94f58ca650fa2e1c84a56e842a578a16
Alexene Tomate <alekzene.cs@gmail.com>
2023-12-31 00:41:11 +0800

    Setup MVC

--- leafly/src/main/kotlin/com/example/leafly/HtmlController.kt
+++ leafly/src/main/kotlin/com/example/leafly/HtmlController.kt
@@ -2,6 +2,7 @@ package com.example.leafly
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
@@ -9,7 +10,7 @@ class HtmlController {
 
     @GetMapping("/")
     fun blog(model: Model): String {
-        model.addAttribute("title", "Leafly")
+        model["title"] = "Leafly"
         return "leafly"
     }
 }
