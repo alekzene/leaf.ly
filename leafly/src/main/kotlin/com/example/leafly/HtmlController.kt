e81e54829a35bfc34a54730c80685d0fa36ba469
Alexene Tomate <alekzene.cs@gmail.com>
2024-01-02 20:01:29 +0800

    update HtmlController.kt

--- leafly/src/main/kotlin/com/example/leafly/HtmlController.kt
+++ leafly/src/main/kotlin/com/example/leafly/HtmlController.kt
@@ -11,6 +11,20 @@ class HtmlController {
     @GetMapping("/")
     fun leafly(model: Model): String {
         model["title"] = "leaf.ly"
+        model["styles-address"] = "/css/styles.css"
         return "leafly"
+    }
+    @GetMapping("/sign-up.mustache")
+    fun signUp(model: Model): String {
+        model["title"] = "sign up"
+        model["styles-address"] = "/css/sign-up-styles.css"
+        return "sign-up"
+    }
+
+    @GetMapping("/log-in.mustache")
+    fun logIn(model: Model): String {
+        model["title"] = "log in"
+        model["styles-address"] = "/css/log-in-styles.css"
+        return "log-in"
     }
 }
