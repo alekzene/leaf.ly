a2f1689bce0486d3cf0e62743204127435565341
Alexene Tomate <alekzene.cs@gmail.com>
2024-01-06 01:15:07 +0800

    update sandbox.kt

--- leafly/src/main/kotlin/com/example/leafly/Sandbox.kt
+++ leafly/src/main/kotlin/com/example/leafly/Sandbox.kt
@@ -15,7 +15,7 @@ fun main() {
     // A comment.
 
     // val: read-only
-    val pigeons = 10 // NOTICE: Kotlin automatically inferred type Int. ("val pigeons: Int")
+    val pigeons = 10 // TYPE INFERENCE: Kotlin automatically inferred type Int. ("val pigeons: Int")
 
     // var: mutable
     var people = 25 /* FIXME: Variable 'people' is assigned but never accessed
@@ -38,12 +38,71 @@ fun main() {
     * If I print pigeons below, its value is still 10.*/
 
     println(pigeons)
+
+    // augmented assignments
+    sticks += 1; sticks -=25; sticks /= 25; sticks *= 5 /* SEMI-COLON USE CASE: to separate statements in one line */
+
+    lists()
 }
 
 // sticks = 1
 
 /* BASIS: "Variables can be declared outside the main() function at the beginning of your program."
+
 * THOUGHT PROCESS: I focused too much on "can be declared outside ... main()". I jumped to the assumption that I
 * would also be able to reuse the variable outside main().
+
 * REALIZATION: The documentation only said I can "declare", not "update the value of", the variable outside main(). */
 
+fun variables() {
+    // variable initialization : to set the initial value of a variable
+    val hiccups_per_hour = 25
+
+    // variable declaration without initialization
+    val weight : Double
+
+    // explicit variable declaration + initialization
+    val warts : UInt = 2u // UInt: Unsigned Integer (No positive or negative sign.)
+
+    // INTERESTING: An escape is considered a Char even if it has two characters.
+    val next_line : Char = '\n'
+
+    /* FIXME: What's the purpose of a Float? How does it differ from a Double? */
+}
+
+fun lists() {
+    val foreverFriends = listOf("Marga", "Inna", "Sophia", "Lai") // NOTE: type inference for elements
+    val moreFriends = mutableListOf("Winona", "Chelsea", "Kim") // Mutable List
+
+    // explicit type declaration
+    val guyFriends : MutableList<String> = mutableListOf("Jerome", "Aeris", "Sean", "Achilles")
+
+    /* FIXED: Is a Mutable List still mutable if it's a val and not a var? Why or why not?
+
+    * ANSWER: It is immutable in the sense that it cannot be re-assigned to any new value (whether it be a new
+    * instance of a list or a completely different value with a different data type). However, it is mutable in a
+    * sense that operations like add() can be done on the mutable list but not on the read-only list.
+
+    * BASICALLY:
+    * read-only (val): prevents reassignment in variable;
+    * read-only (list): prevents element alteration in list*/
+
+    // casting mutable list to read-only list
+    val moreFriendsLocked : List<String> = moreFriends
+
+    // accessing elements in a list
+    println("${foreverFriends[0]} is a huge fan of Le Sserafim's Heo Yunjin.")
+    println("${foreverFriends.last()}'s goal is to become a lawyer.")
+
+    // count elements in a list
+    println("I have ${foreverFriends.count()} childhood girl best friends.")
+
+    // in operator : to check if an element is in the list
+    println("It is ${"Jerome" in guyFriends} that ${guyFriends[0]} is a guy friend of mine.")
+
+    // extension functions
+    guyFriends.add("James"); guyFriends.remove("Achilles")
+
+    /* IDEA: Since maps require unique keys and sets contain unique values, I can use a set to set the keys in a map.
+    * Downside is I might have a hard time mapping values to the keys because of the lack of order in a set. */
+}
